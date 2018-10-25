9a97ed55adcd228b9a905f4163a2144a3e316db6
wellingtondevops <wellingtondevops@gmail.com>
2018-10-24 22:37:04 -0300

    Inicio do Projeto

---
+++ src/main/java/com/wellingtondevops/cursomc/CursomgApplication.java
@@ -0,0 +1,12 @@
+package com.wellingtondevops.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomgApplication.class, args);
+	}
+}
